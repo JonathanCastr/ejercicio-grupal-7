d3d69cff7d9aa597ced1f2da1ab392a2ec780c7e
galiardi <galiardi.dev@gmail.com>
2023-04-18 22:18:09 -0400

    Modifica funciones.js

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -2,6 +2,7 @@ const form = document.getElementById('form');
 const email = document.getElementById('inputEmail');
 const password = document.getElementById('inputPassword');
 const checkbox = document.getElementById('checkBox');
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (email.value == "") {
@@ -9,12 +10,12 @@ form.addEventListener('submit', (e) => {
     return false;
   }
   if (password.value == "") {
-      alert("La contraseña no puede estar vacía");
-      return false;
-    }
-  if (checkbox.value == "" ) {
-      alert("El mensaje no puede estar vacío");
-      return false;
-    }
+    alert("La contraseña no puede estar vacía");
+    return false;
+  }
+  if (checkbox.value == "") {
+    alert("El mensaje no puede estar vacío");
+    return false;
+  }
 }
 );
