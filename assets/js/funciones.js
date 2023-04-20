a3af2aab1cc4afd68871e6aaed7ccab2fb1bdb4a
JonathanCastr <98371104+JonathanCastr@users.noreply.github.com>
2023-04-20 19:50:29 -0400

    Partes a trabajar por cada uno

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,3 +1,4 @@
+<!-- Pablo  -->
 const form = document.getElementById('form');
 const email = document.getElementById('inputEmail');
 const password = document.getElementById('inputPassword');
@@ -19,3 +20,4 @@ form.addEventListener('submit', (e) => {
   }
 }
 );
+<!-- /Pablo -->
